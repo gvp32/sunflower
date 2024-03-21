b35639a3e9e004190edac08eaa17151bc1698acb
wjj9132 <974442421@qq.com>
2024-03-21 18:58:43 +0800

    添加webpack配置文件

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+//配置webpack
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    }
+}
